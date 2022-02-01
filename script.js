b704a5a0a33059c223bcc6a30983a7b10d43a1fc
konoleo <leokono4580@gmail.com>
2022-02-01 23:56:24 +0900

    3rd comit

--- script.js
+++ script.js
@@ -5,40 +5,45 @@ function getClosestElem(num, arr) {
 	});
 }
 
+const fileDiv = document.getElementById("fileDiv");
+const fileLabel = document.getElementById("fileLabel");
+const fileInputE = document.getElementById("inputMidi");
+const controlers = document.getElementById("controlers");
 const picoAudio = new PicoAudio();
-const controler = document.getElementById("controler");
+const playPause = document.getElementById("playPause");
+const skipPrevious = document.getElementById("skipPrevious");
+const replay5 = document.getElementById("replay5");
 const pianoRoll = document.getElementById("pianoroll");
+var chorusNum = 0;
 var scrollAnime;
 var minPitch;
 
-// ファイル読み込み部分
-const fileInputE = document.getElementById("inputMidi");
-fileInputE.addEventListener("change", async () => {
-	const file = fileInputE.files[0];
+fileDiv.style.height = window.innerHeight + "px";
+
+async function readMidi(e) {
+	const file = e.target.files[0];
 	const reader = new FileReader();
 	reader.onload = () => {
 		const smf = new Uint8Array(reader.result);
 		const smfData = picoAudio.parseSMF(smf);
 		// データセット
 		picoAudio.setData(smfData);
+		// ノート初期化
+		pianoRoll.innerHTML = "";
 		// ノート作成
-		const mainPitches = smfData.channels[0].notes.map(note => note.pitch);
+		const mainChannel = smfData.channels[chorusNum];
+		const mainPitches = mainChannel.notes.map(note => note.pitch);
 		const min = Math.min(...mainPitches);
 		const max = Math.max(...mainPitches);
 		minPitch = (min - (300 / 7 - (max - min)) / 2) * 7;
-		smfData.channels.forEach((channel, i) => {
-			channel.notes.forEach(note => {
-				const newNote = document.createElement("div");
-				newNote.classList.add("note");
-				if (i !== 0) {
-					newNote.classList.add("hide");
-				}
-				// const soundData = Object.values(freqList).find(value => value.midinote === note.pitch);
-				newNote.style.left = note.start / 7 + "px";
-				newNote.style.bottom = note.pitch * 7 - minPitch + "px";
-				newNote.style.width = (note.stop - note.start) / 7 + "px";
-				pianoRoll.appendChild(newNote);
-			});
+		mainChannel.notes.forEach(note => {
+			const newNote = document.createElement("div");
+			newNote.classList.add("note");
+			// const soundData = Object.values(freqList).find(value => value.midinote === note.pitch);
+			newNote.style.left = note.start / 7 + "px";
+			newNote.style.bottom = note.pitch * 7 - minPitch + "px";
+			newNote.style.width = (note.stop - note.start) / 7 + "px";
+			pianoRoll.appendChild(newNote);
 		});
 		// スクロール設定
 		let keyframes = [{
@@ -63,26 +68,73 @@ fileInputE.addEventListener("change", async () => {
 		scrollAnime.pause();
 	};
 	reader.readAsArrayBuffer(file);
-	// 表示・非表示
-	document.getElementById("inputDiv").setAttribute("hidden", "");
-	document.getElementsByTagName("main")[0].removeAttribute("hidden");
-});
+}
 
-// コントロール
-controler.addEventListener("click", () => {
+// ファイル読み込み
+fileInputE.addEventListener("change", (e) => {
+	readMidi(e);
+	// 表示・非表示
+	fileDiv.setAttribute("hidden", "");
+	document.getElementsByTagName("main")[0].removeAttribute("hidden");
+	fileLabel.innerHTML = fileLabel.innerHTML.replace("ファイルを選択", "別の曲を練習する");
+	controlers.appendChild(fileLabel);
+	// 新しい場所にイベント追加
+	document.querySelector("#controlers #inputMidi").addEventListener("change", e => readMidi(e));
+}, {once: true});
+
+// 再生/停止
+playPause.addEventListener("click", () => {
 	picoAudio.init();
-	if (controler.getAttribute("data-playing") === "false") {
+	if (playPause.getAttribute("data-playing") === "false") {
 		// 再生中
-		controler.textContent = "一時停止";
-		controler.setAttribute("data-playing", "true");
+		playPause.textContent = "pause";
+		playPause.setAttribute("title", "停止");
+		playPause.setAttribute("data-playing", "true");
 		picoAudio.play();
 		scrollAnime.play();
 	} else {
 		// 停止中
-		controler.textContent = "再生";
-		controler.setAttribute("data-playing", "false");
+		playPause.textContent = "play_arrow";
+		playPause.setAttribute("title", "再生");
+		playPause.setAttribute("data-playing", "false");
 		picoAudio.pause();
 		scrollAnime.pause();
+	}
+});
+
+// 最初に戻す
+skipPrevious.addEventListener("click", () => {
+	picoAudio.init();
+	picoAudio.initStatus();
+	scrollAnime.cancel();
+	document.querySelectorAll(".voice").forEach(elem => {
+		elem.remove();
+	});
+	if (playPause.getAttribute("data-playing") === "true") {
+		playPause.click();
+	}
+});
+
+// 5秒戻す
+replay5.addEventListener("click", () => {
+	const test = scrollAnime.currentTime;
+	let before5Time = test - 5000;
+	if (before5Time < 0) {
+		before5Time = 0
+	}
+	scrollAnime.currentTime = before5Time;
+	scrollAnime.startTime = before5Time;
+	// scrollAnime.pause();
+	picoAudio.setStartTime(before5Time);
+	console.log("scrollAnime.currentTime: ", test);
+	console.log("scrollAnime.currentTime: ", scrollAnime.currentTime);
+	document.querySelectorAll(".voice").forEach(elem => {
+		if (Number(elem.style.left.replace("px", "")) * 7 > picoAudio.getTiming(before5Time)) {
+			elem.remove();
+		}
+	});
+	if (playPause.getAttribute("data-playing") === "true") {
+		playPause.click();
 	}
 });
 
@@ -103,28 +155,25 @@ controler.addEventListener("click", () => {
 		const index = frequencyData.indexOf(maxValue);
 		if (maxValue > 200 && index !== 0) {
 			const hz = Math.round(index * unit);
-			var closestHz = getClosestElem(hz, Object.keys(freqList));
-			var soundData = freqList[closestHz];
-		} else {
-			return;
-		}
-		const newestNote = pianoRoll.querySelector(":scope > .note.voice:last-of-type");
-		const playTime = getCurrentTime();
-		if (!newestNote || Number(newestNote.getAttribute("data-midinote")) !== soundData.midinote) {
-			// 新しい音
-			const newNote = document.createElement("div");
-			newNote.classList.add("note", "voice");
-			newNote.style.left = picoAudio.getTiming(playTime) / 7 + "px";
-			newNote.style.bottom = soundData.midinote * 7 - minPitch + "px";
-			newNote.style.width = 0;
-			newNote.setAttribute("data-midinote", soundData.midinote);
-			pianoRoll.appendChild(newNote);
-		} else {
-			// 前の音の続き
-			const startTime = Number(newestNote.style.left.replace("px", ""));
-			console.log((playTime - startTime) * 7);
-			newestNote.style.width = Number(newestNote.style.width.replace("px", "")) + (playTime - startTime) * 7 + "px";
-			console.log(soundData.japanese);
+			const closestHz = getClosestElem(hz, Object.keys(freqList));
+			const soundData = freqList[closestHz];
+			const newestNote = pianoRoll.querySelector(":scope > .note.voice:last-of-type");
+			const playTiming = getCurrentTiming();
+			if (!newestNote || Number(newestNote.getAttribute("data-midinote")) !== soundData.midinote) {
+				// 新しい音
+				const newNote = document.createElement("div");
+				newNote.classList.add("note", "voice");
+				newNote.style.left = playTiming / 7 + "px";
+				newNote.style.bottom = soundData.midinote * 7 - minPitch + "px";
+				newNote.style.width = 0;
+				newNote.setAttribute("data-midinote", soundData.midinote);
+				newNote.setAttribute("data-start", playTiming);
+				pianoRoll.appendChild(newNote);
+			} else {
+				// 前の音の続き
+				const startTiming = Number(newestNote.getAttribute("data-start"));
+				newestNote.style.width = (playTiming - startTiming) / 7 + "px";
+			}
 		}
 	}
 
@@ -137,8 +186,8 @@ controler.addEventListener("click", () => {
 	});
 }());
 
-function getCurrentTime() {
-	return (picoAudio.context ? picoAudio.context.currentTime : 0) - picoAudio.states.startTime;
+function getCurrentTiming() {
+	return (picoAudio.context ? picoAudio.getTiming(picoAudio.context.currentTime) : 0) - picoAudio.getTiming(picoAudio.states.startTime);
 }
 
 // ピアノロール
